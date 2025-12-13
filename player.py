62a515a637cef92194c15ae09244bbb98a69a76e
brandon-sherwood <sherwood.brandonlee@gmail.com>
2025-12-13 08:21:12 -0700

    Impletmented forward and backward movement for player ship

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ from circleshape import CircleShape
 from constants import PLAYER_RADIUS
 from constants import LINE_WIDTH
 from constants import PLAYER_TURN_SPEED
+from constants import PLAYER_SPEED
 
 
 class Player(CircleShape):
@@ -13,7 +14,6 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
 
-    # in the Player class
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(
@@ -26,6 +26,12 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -33,3 +39,7 @@ class Player(CircleShape):
             self.rotate(-1 * dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-1 * dt)
