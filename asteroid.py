3336df2cd052f61ab211c3c234839bd8f3b109f5
brandon-sherwood <sherwood.brandonlee@gmail.com>
2025-12-15 07:20:49 -0700

    Added split method when large or medium asteroids are shot

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -13,3 +16,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+        first_new_velocity = self.velocity.rotate(random_angle)
+        second_new_velocity = self.velocity.rotate(-1 * random_angle)
+        old_radius = self.radius
+        new_radius = old_radius - ASTEROID_MIN_RADIUS
+        first_new_asteroid = Asteroid(
+            self.position.x, self.position.y, new_radius)
+        second_new_asteroid = Asteroid(
+            self.position.x, self.position.y, new_radius)
+        first_new_asteroid.velocity = first_new_velocity * 1.2
+        second_new_asteroid.velocity = second_new_velocity * 1.2
